a1eca0e804f46c91b197b0f95124bb50fba5fbee
AjizahNur <nrajzh2@gmail.com>
2026-07-06 15:15:58 +0700

    Add files via upload

---
+++ liveCoding2/src/BankBCA.java
@@ -0,0 +1,15 @@
+public class BankBCA extends Bank {
+    @Override
+    public void sukuBunga() {
+        System.out.println("Suku Bunga BCA adalah 4.5%");
+    }
+    @Override
+    public void transferUang(int jumlah, String rekeningTujuan, String bankTujuan) {
+        System.out.println("Transfer Rp" + jumlah + " ke rekening " + rekeningTujuan + "di bank BCA");
+    }
+    @Override
+    public int biayaTransfer() {
+        return 3000;
+    }
+    
+}
